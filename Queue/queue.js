2f1274f248f4a7d3d0e44223ba475d0b76238959
sheeraz ali <hacker.ascool@gmail.com>
2019-07-09 10:47:32 -0700

    added stacks and queues impelementation today

---
+++ Queue/queue.js
@@ -0,0 +1,57 @@
+
+class Node{
+    constructor(v){
+        this.val = v;
+        this.next= null;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+peek(){
+    console.log(this.first)
+    return this.first;
+
+}
+
+enqueue(v){
+    const newNode = new Node(v);
+    if ( this.length === 0 ){
+        this.first = newNode;
+        this.last = newNode;
+    }else{
+        this.last.next = newNode;
+        this.last = newNode;
+    }
+    this.length++;
+}
+dequeue(){
+    if ( !this.first){
+        return null
+    }
+    if ( this.first === this.last ){
+        this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+}
+}
+
+
+
+const myQueue = new Queue();
+myQueue.enqueue('joys');
+myQueue.enqueue('hulk');
+myQueue.enqueue('nope');
+myQueue.enqueue('runaway');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.peek();
+myQueue.dequeue();
+myQueue.peek();
+
---
+++ Queue/queueStacks.js
@@ -0,0 +1,49 @@
+class Node{
+    constructor(v){
+        this.val = v;
+        this.next= null;
+    }
+}
+
+class StacksQueue{
+    constructor(v){
+        this.data = []
+    };
+
+    enqueue(v){
+        const newnode = new Node(v);
+        this.data.push(newnode);
+        console.log(this)
+}
+    dequeue(){
+        this.data.splice(0,1);
+    }
+    peek(){
+
+        console.log(this.data)
+    }
+}
+
+let lol = new StacksQueue;
+lol.enqueue('udemy');
+lol.enqueue('google');
+lol.enqueue('youtube');
+lol.enqueue('golang');
+lol.enqueue('koriala');
+lol.dequeue()
+lol.peek()
+console.log("-----------------------------------------")
+lol.dequeue()
+lol.peek()
+console.log("-----------------------------------------")
+
+lol.dequeue()
+lol.peek()
+console.log("-----------------------------------------")
+
+lol.dequeue()
+lol.peek()
+console.log("-----------------------------------------")
+
+lol.dequeue()
+lol.peek()
