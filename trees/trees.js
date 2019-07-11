bba8e262ebb32fcb62cb29381d93661601988f10
sheeraz ali <hacker.ascool@gmail.com>
2019-07-11 11:32:02 -0700

    also added bainary  search tree description

--- trees/trees.js
+++ trees/trees.js
@@ -1,10 +1,15 @@
-level 0: 2^0 = 1;
-level 1: 2^1 = 2;
-level 2: 2^2 = 4;
-level 3: 2^3 = 8;
+// level 0: 2^0 = 1;
+// level 1: 2^1 = 2;
+// level 2: 2^2 = 4;
+// level 3: 2^3 = 8;
 
-# number of nodes = 2^H -1; h being the height 
-log nodes = steps;
+// # number of nodes = 2^H -1; h being the height 
+// log nodes = steps;
 
-log 100 = 2
-10^2 = 100;
+// log 100 = 2
+// 10^2 = 100;
+
+// bainary search tree - is really good with searching 
+// all the right side of bst should be greater then the root node 
+// and all the left should be decreaseing 
+// nodes can have only two children like not odd numbers it should be a perfect bainary tree
