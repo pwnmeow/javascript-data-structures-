0b4be2daa5b586047565209dcf1b13405ad2bcc2
sheeraz ali <hacker.ascool@gmail.com>
2019-07-12 20:00:29 -0700

    graphs and arrays added

---
+++ graphs/types of lists .js
@@ -0,0 +1,15 @@
+// types of lists we can use for graphs
+//Edge list 
+const graph = [[0,2],[2,3],[2,10],[1,3]];
+//Adjecent List 
+const graph = [[2],[2,3], [0,1,3],[1,2]];
+
+//Adjecent Matrix
+const graph = {
+    0: [0,0,1,0],
+    1: [0,0,1,1],
+    2: [1,1,0,1],
+    3: [0,1,1,0]
+}
+
+
