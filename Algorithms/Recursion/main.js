42b6b3155a1f17d2041ac081f62df028ba4daf21
sheeraz ali <hacker.ascool@gmail.com>
2019-07-13 19:09:56 -0700

    recurrsion step one

---
+++ Algorithms/Recursion/main.js
@@ -0,0 +1,13 @@
+//BaseCase for recurrsion 
+let counter = 0;
+function inception(){
+    console.log(counter)
+    if (counter > 3){
+        return 'done';
+    }
+    counter++
+    return inception();
+}
+
+
+inception();
